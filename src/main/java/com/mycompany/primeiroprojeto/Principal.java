1ed988166bc45e1bc566db5c963bedb99e1d3e9a
kaiopostal <83304473+kaiopostal@users.noreply.github.com>
2021-05-17 14:44:52 -0300

    Adicionado Olá mundo

--- src/main/java/com/mycompany/primeiroprojeto/Principal.java
+++ src/main/java/com/mycompany/primeiroprojeto/Principal.java
@@ -12,6 +12,6 @@ package com.mycompany.primeiroprojeto;
 public class Principal {
     //Método principal
     public static void main(String[] args) {
-        
+    System.out.println("Olá mundo"   
     }
 }
